43c726d617d3988f66576f5a1dada70fa004bea7
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-08-07 13:01:46 +0200

    define home in views.py

--- news/views.py
+++ news/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    country = request.GET.get('country', 'us')
+    category = request.GET.get('category', 'general')
+    page = int(request.GET.get('page', 1))
+    
+    news = get_news(country=country, category=category, page=page)
+    articles = news.get('articles', [])
+
+    return render(request, 'news/home.html', {
+        'news': articles,
+        'country': country,
+        'category': category,
+        'page': page,
+        'has_more': len(articles) == 20 
+    })
